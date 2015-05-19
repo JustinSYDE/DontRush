7e66ad3e287ccec9a316e85d968d2aafcd48bef6
Justin Wong <justinwong12337@gmail.com>
2015-05-18 22:22:31 -0700

    Initial commit

---
+++ DontRush/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DontRush
+//
+//  Created by Justin Wong on 2015-05-18.
+//  Copyright (c) 2015 justinSYDE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DontRush/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DontRush
+//
+//  Created by Justin Wong on 2015-05-18.
+//  Copyright (c) 2015 justinSYDE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
