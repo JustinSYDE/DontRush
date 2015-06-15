c2fbed20c22484aef75c8bba823c3abccd341eae
Justin Wong <justinwong12337@gmail.com>
2015-06-14 19:19:12 -0700

    Fixing a bug where user was able to interact with the game view while correct answers were being highlighted

--- DontRush/GameViewController.m
+++ DontRush/GameViewController.m
@@ -221,6 +221,7 @@
 }
 
 - (void)highlightMissedShapes {
+    self.gameView.userInteractionEnabled = NO;
     self.gameView.layer.borderColor = [[self colorFromHexString:@"#dbc8b2"] CGColor];
     [self.questionView updateQuestionLabel:[[NSAttributedString alloc] initWithString:@"Lol"]];
     NSString *questionColor = [self.game.questionObject allKeys][0];
@@ -505,7 +506,6 @@
 }
 
 - (void)endGame {
-    [self.gameView resetViewPositionAndTransformations];
     self.gameView.userInteractionEnabled = NO;
     [self gameOver];
     [self.game resetCurrentGameData];
